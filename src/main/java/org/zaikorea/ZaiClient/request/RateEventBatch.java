38a75f6fb8bc281a44ce638dbb9c840d58c1df17
Hyoung Uk Sul <ssstuartss@gmail.com>
2022-05-11 13:23:05 +0900

    Update RateEventBatch.java

--- src/main/java/org/zaikorea/ZaiClient/request/RateEventBatch.java
+++ src/main/java/org/zaikorea/ZaiClient/request/RateEventBatch.java
@@ -40,12 +40,12 @@ public class RateEventBatch extends EventBatch {
         return events;
     }
 
-    public void deleteEventItem(String itemId) throws LoggedEventBatchException, ItemNotFoundException {
-        super.deleteEventItem(itemId);
-    }
-
     public void addEventItem(String itemId, double rate) throws LoggedEventBatchException, BatchSizeLimitExceededException {
         super.addEventItem(itemId, Double.toString(rate));
+    }
+    
+    public void deleteEventItem(String itemId) throws LoggedEventBatchException, ItemNotFoundException {
+        super.deleteEventItem(itemId);
     }
 
     public void deleteEventItem(String itemId, double rate) throws LoggedEventBatchException, ItemNotFoundException {
