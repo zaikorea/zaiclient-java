ca3a14d9a52d0915fd9ef0e3fae513942033efc5
Hyoung Uk Sul <ssstuartss@gmail.com>
2022-05-11 13:19:57 +0900

    Update RateEventBatch.java

--- src/main/java/org/zaikorea/ZaiClient/request/RateEventBatch.java
+++ src/main/java/org/zaikorea/ZaiClient/request/RateEventBatch.java
@@ -1,11 +1,12 @@
 package org.zaikorea.ZaiClient.request;
 
-import org.zaikorea.ZaiClient.configs.Config;
-import org.zaikorea.ZaiClient.exceptions.ItemNotFoundException;
-import org.zaikorea.ZaiClient.exceptions.BatchSizeLimitExceededException;
-import org.zaikorea.ZaiClient.exceptions.LoggedEventBatchException;
-
 import java.util.ArrayList;
+import java.util.List;
+
+import org.zaikorea.ZaiClient.configs.Config;
+import org.zaikorea.ZaiClient.exceptions.BatchSizeLimitExceededException;
+import org.zaikorea.ZaiClient.exceptions.ItemNotFoundException;
+import org.zaikorea.ZaiClient.exceptions.LoggedEventBatchException;
 
 public class RateEventBatch extends EventBatch {
 
@@ -24,26 +25,30 @@ public class RateEventBatch extends EventBatch {
     }
 
     @Override
-    public ArrayList<Event> getEventList() {
-        ArrayList<Event> events = new ArrayList<>();
+    public List<Event> getEventList() {
+        List<Event> events = new ArrayList<>();
 
         for (int i = 0; i < this.itemIds.size(); i++) {
-            Event event = new RateEvent(this.userId, this.itemIds.get(i), Double.parseDouble(this.eventValues.get(i)), this.timestamp + Config.epsilon * i);
-            events.add(event);
+            events.add(new RateEvent(
+                this.userId, 
+                this.itemIds.get(i), 
+                Double.parseDouble(this.eventValues.get(i)), 
+                this.timestamp + Config.epsilon * i
+            ));
         }
+        
         return events;
-    }
-
-    public void addEventItem(String itemId, double rate) throws LoggedEventBatchException, BatchSizeLimitExceededException {
-        super.addEventItem(itemId, Double.toString(rate));
     }
 
     public void deleteEventItem(String itemId) throws LoggedEventBatchException, ItemNotFoundException {
         super.deleteEventItem(itemId);
     }
 
+    public void addEventItem(String itemId, double rate) throws LoggedEventBatchException, BatchSizeLimitExceededException {
+        super.addEventItem(itemId, Double.toString(rate));
+    }
+
     public void deleteEventItem(String itemId, double rate) throws LoggedEventBatchException, ItemNotFoundException {
         super.deleteEventItem(itemId, Double.toString(rate));
     }
-
 }
