702651e6a72f61fa08bc8884bfe3ce8c0843b69e
Hyoung Uk Sul <ssstuartss@gmail.com>
2022-05-11 12:35:13 +0900

    Update ItemNotFoundException.java

--- src/main/java/org/zaikorea/ZaiClient/exceptions/ItemNotFoundException.java
+++ src/main/java/org/zaikorea/ZaiClient/exceptions/ItemNotFoundException.java
@@ -1,6 +1,7 @@
 package org.zaikorea.ZaiClient.exceptions;
 
 public class ItemNotFoundException extends Exception {
+
     public ItemNotFoundException() {
         super("The operation tried to delete a nonexistent item.");
     }
